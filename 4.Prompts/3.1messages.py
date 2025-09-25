79a6c6f8f30c7bf45c9a1495e64e5f6ea4590b47
Uzmaa7 <uk9971khan@gmail.com>
2025-09-25 13:05:07 +0530

    3types of langchain messages

---
+++ 4.Prompts/3.1messages.py
@@ -0,0 +1,24 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id =  "openai/gpt-oss-20b",
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant"),
+    HumanMessage(content="Tell me about langchain")
+]
+
+result = model.invoke(messages)
+
+messages.append(AIMessage(content=result.content))
+print(messages)
