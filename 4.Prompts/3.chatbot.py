3e2a37fafa87a8be4b9d3d9492571e586c651bbc
Uzmaa7 <uk9971khan@gmail.com>
2025-09-25 13:12:34 +0530

    labelled the messages as system msg, human msg or Ai msg

--- 4.Prompts/3.chatbot.py
+++ 4.Prompts/3.chatbot.py
@@ -1,6 +1,10 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+
+
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
@@ -10,17 +14,20 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm=llm)
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful ai assistant"),
+
+]
 
 while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
 
     if user_input == "exit":
         break
 
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
 
     print("AI: ", result.content)
 
